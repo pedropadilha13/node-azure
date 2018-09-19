3b0800a572b6d386e26f3b9365cffc88866ec5b6
Pedro Padilha <pedropadilha@me.com>
2018-09-19 14:45:43 -0300

    asjdasd

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/:a', function(req, res, next) {
+  res.send(req.params.a);
+});
+
 module.exports = router;
